acc0c39b54a5733e3e9d9260df89e0963a13b7e4
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 22:19:25 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
-
-
-const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App store={store}/>
-  </Provider>,
-  document.getElementById('root')
-);
+import uuid from "uuid";
+ 
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+ 
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(book => book.id === action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+ 
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+ 
+    default:
+      return state;
+  }
+}
