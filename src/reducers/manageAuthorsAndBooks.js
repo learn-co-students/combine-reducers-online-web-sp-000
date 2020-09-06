d3adc773fdd19ba8cd80fd8090130af6e43904ae
Soner Mezgitci <mezgitci9@gmail.com>
2020-09-06 00:24:48 -0400

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,27 +1,3 @@
-import { combineReducers } from "redux";
- 
-const rootReducer = combineReducers({
-  authors: authorsReducer,
-  books: booksReducer
-});
- 
-export default rootReducer;
- 
-function booksReducer(state = [], action) {
-  let idx;
-  switch (action.type) {
-    case "ADD_BOOK":
-      return [...state, action.book];
- 
-    case "REMOVE_BOOK":
-      idx = state.findIndex(book => book.id  === action.id)
-      return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
-    default:
-      return state;
-  }
-}
-
 import uuid from "uuid";
  
 function authorsReducer(state = [], action) {
