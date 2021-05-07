9fb94b3af5f6960596ef3da6850590f85996acbf
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-07 08:44:47 -0700

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,4 +1,5 @@
 export default function bookApp(
+  //top-level keys 
   state = {
     authors: [],
     books: []
@@ -6,10 +7,13 @@ export default function bookApp(
   action
 ) {
   let idx;
+  //reducers
   switch (action.type) {
     case "ADD_BOOK":
       return {
         ...state,
+        //... spread operator 
+        authors: [...state.authors],
         books: [...state.books, action.book]
       };
 
@@ -17,11 +21,14 @@ export default function bookApp(
       idx = state.books.findIndex(book => book.id === action.id);
       return {
         ...state,
+        authors: [...state.authors],
         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
       };
 
     case "ADD_AUTHOR":
       return {
+        //here in ...state we are referencing old state.books not a new instance 
+        //this is not immutable (meaning the state can be changed)
         ...state,
         authors: [...state.authors, action.author]
       };
@@ -30,6 +37,7 @@ export default function bookApp(
       idx = state.authors.findIndex(author => author.id === action.id);
       return {
         ...state,
+        books: [...state.books],
         authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
       };
 
@@ -37,3 +45,5 @@ export default function bookApp(
       return state;
   }
 }
+
+// when we couple resources it is not ideal for separation of concerns because we are placing each resource in the same reducer 
